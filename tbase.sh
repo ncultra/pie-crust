3e7c8a6ef3bd51ba20a9fd581951958992e80291
Mike Day <ncmike@ncultra.org>
2017-10-17 13:09:23 -0400

    silly bash utility to get the text base of a linux process. It will also print the pid, and mapped file. As of now it doesn't have enough functionality to deserve to exist as a separate command (just use head | cut like the script does). Plans are to drop map-related functions into this script in the future, so it (hopefully) will get some more stuff.

---
+++ tbase.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# tbase.sh pid (or none for self)
+
+PID=self
+PROGRAM=$0
+QUIET=0
+usage() {
+    echo "$PROGRAM"
+    echo "  [--pid]=<process id> of maps to read, or leave empty to read self/maps"
+    echo "  [--config]=<config file> (not yet in use)"
+    echo "  [--gdb]  run under gdb (not yet implemented)"
+    
+    exit 1
+}
+
+until [ -z "$1" ]; do	 
+    case "${1:0:2}" in
+	"--")
+	    case "${1:2:3}" in 
+		"pid") PID="${1##--pid=}";;
+		"con") CONFIG_FILE="${1##--config=}";;
+		"gdb") RUN_GDB=1;;
+		"qui") QUIET=1;;
+		"hel") usage ;;
+	    esac ;;
+    esac
+    shift;
+done
+TEXT_BASE="$(head -n1 /proc/$PID/maps | grep 'r-xp' | cut -s -d '-' -f 1)"
+FILE="/$(head -n1 /proc/$PID/maps | grep 'r-xp' | cut -s -d '/'  -f 2-)" 
+if (( $QUIET == 0 )) ; then 
+    echo "pid    text base       file name"
+    echo "---------------------------------"
+fi
+echo "$PID   $TEXT_BASE      $FILE      "
